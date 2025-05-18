f04f9339de90d782a4fc2fe26f5e101b044b069f
harmon1c <80057115+harmon1c@users.noreply.github.com>
2025-05-18 20:40:30 +0300

    Tests and misc (#16)
    * docs(RSS-ECOMM-2_25): edit readme file, fix images path for deploy
    
    * test(RSS-ECOMM-2_25): testing configs, add button, login form, api client, hooks tests

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['your-image-domain.com'],
+    domains: ['rssecom.netlify.app'],
+    remotePatterns: [
+      {
+        hostname: 'rssecom.netlify.app',
+        protocol: 'https',
+      },
+    ],
+    unoptimized: true,
   },
+  output: 'standalone',
   reactStrictMode: true,
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   webpack(config) {
