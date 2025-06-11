ccf5078a8f4bbeb27f7f957c17562c20a9f55a21
Denis Yeresko <91375056+dyeresko@users.noreply.github.com>
2025-06-11 15:03:16 +0300

    Feat(rss ecomm 4 18)/add clear cart feature (#47)
    * feat: add possibility of adding products on product details page
    
    * fix: change import
    
    * feat: add promo code feature
    
    * feat: add possibility to clear cart

--- src/components/cart/CartPageContent.tsx
+++ src/components/cart/CartPageContent.tsx
@@ -86,20 +86,32 @@ export default function CartPageContent(): JSX.Element {
       const response = await customerClient.applyDiscountCode(code);
       if (response?.discountOnTotalPrice?.discountedAmount.centAmount) {
         setDiscount(response?.discountOnTotalPrice?.discountedAmount.centAmount);
-        console.log(discount);
       }
       if (response?.totalPrice.centAmount) {
         setDiscountedPrice(response?.totalPrice.centAmount);
-        console.log(discountedPrice);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
+  const handleClearCartClick = async (): Promise<void> => {
+    try {
+      if (customerClient) {
+        await customerClient.clearCart();
+        await refreshCart();
+      }
+    } catch (error) {
+      console.error('Failed to clear cart:', error);
+    }
+  };
+
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+        <Button onClick={() => handleClearCartClick()}>Clear Cart</Button>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <Card className="col-span-2">
           <CardHeader>
@@ -153,7 +165,6 @@ export default function CartPageContent(): JSX.Element {
             );
           })}
         </div>
-
         <div>
           <Card className="p-6">
             <h2 className="text-xl font-medium mb-4">Order Summary</h2>
